43f02ef359b69f15e08cc71875b9d8d275d1a995
Ильин Олег <oleg.ilyin@infotecs.ru>
2022-01-12 18:42:27 +0300

    Добавлена генерация swagger.json Swashbuckle.AspNetCore заменен на NSwag.AspNetCore для генерации спецификации

---
+++ Infotecs.MobileMonitoring/Contracts/StatisticsContract.cs
@@ -0,0 +1,9 @@
+namespace Infotecs.MobileMonitoring.Contracts;
+
+public class StatisticsContract
+{
+    public Guid Id { get; set; }
+    public string UserName { get; set; }
+    public string ClientVersion { get; set; }
+    public string OsName { get; set; }
+}
--- Infotecs.MobileMonitoring/Controllers/StatisticsController.cs
+++ Infotecs.MobileMonitoring/Controllers/StatisticsController.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Infotecs.MobileMonitoring.Contracts;
 using Infotecs.MobileMonitoring.Interfaces;
 using Infotecs.MobileMonitoring.Models;
+using Mapster;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,23 +21,25 @@ namespace Infotecs.MobileMonitoring.Controllers
         {
             this.statisticsService = statisticsService;
         }
-
+        
         [HttpGet("list")]
         public Task<ICollection<StatisticsModel>> GetList(CancellationToken cancellationToken)
         {
             return statisticsService.GetListAsync(cancellationToken);
         }
         [HttpPut("create")]
-        public async Task<IActionResult> Create(StatisticsModel statisticsModel, CancellationToken cancellationToken)
+        public async Task<IActionResult> Create(StatisticsContract statisticsContract, CancellationToken cancellationToken)
         {
-            statisticsModel.CreatedAt = DateTime.Now;
+            //statisticsModel.CreatedAt = DateTime.Now;
+            var statisticsModel = statisticsContract.Adapt<StatisticsModel>();
             await statisticsService.CreateAsync(statisticsModel, cancellationToken);
             return NoContent();
         }
 
         [HttpPut("update")]
-        public async Task<IActionResult> Update(StatisticsModel statisticsModel, CancellationToken cancellationToken)
+        public async Task<IActionResult> Update(StatisticsContract statisticsContract, CancellationToken cancellationToken)
         {
+            var statisticsModel = statisticsContract.Adapt<StatisticsModel>();
             await statisticsService.UpdateAsync(statisticsModel,cancellationToken);
             return NoContent();
         }

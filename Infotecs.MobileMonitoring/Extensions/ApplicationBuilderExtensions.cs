9704b202f48526871924410969ffcb261f34f135
Ильин Олег <oleg.ilyin@infotecs.ru>
2022-01-13 16:21:36 +0300

    Добавлено: - сброс данных в mongodb - конфигрурация mongodb и seq через appsettings.json - поддержка docker

---
+++ Infotecs.MobileMonitoring/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,17 @@
+namespace Infotecs.MobileMonitoring.Extensions;
+
+public static class ApplicationBuilderExtensions
+{
+    public static IApplicationBuilder UseSwaggerInfrastructure(this IApplicationBuilder app)
+    {
+        // app.UseSwagger();
+        // app.UseSwaggerUI();
+        // app.UseOpenApi(c =>
+        // {
+        //     c.
+        // });
+        app.UseOpenApi();
+        app.UseSwaggerUi3();
+        return app;
+    }
+}
--- Infotecs.MobileMonitoring/Extensions/ServiceCollectionExtensions.cs
+++ Infotecs.MobileMonitoring/Extensions/ServiceCollectionExtensions.cs
@@ -9,12 +9,13 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddSerilogServices(
         this IServiceCollection services, 
-        LoggerConfiguration configuration)
+        LoggerConfiguration configuration,
+        string seqConnection)
     {
         Log.Logger = configuration
             .MinimumLevel.Debug()
             .WriteTo.Console()
-            .WriteTo.Seq("http://localhost:5341")
+            .WriteTo.Seq(seqConnection)
             // .File(
             //     path: AppDomain.CurrentDomain.BaseDirectory + "/logs/log-.txt",
             //     retainedFileCountLimit: 5,
@@ -35,6 +36,31 @@ public static class ServiceCollectionExtensions
             {
                 model.CreatedAt = DateTime.Now;
             });
+        return services;
+    }
+
+    public static IServiceCollection AddNSwag(this IServiceCollection services)
+    {
+        services.AddOpenApiDocument(doc => 
+            doc.PostProcess = document =>
+            {
+                document.Info.Title = "Infotecs Monitoring Service API";
+            });
+        //builder.Services.AddOpenApiDocument();
+        //builder.Services.AddSwaggerDocument();
+        // builder.Services.AddSwaggerGen(c =>
+        // {
+        //     c.SwaggerDoc("v1", new OpenApiInfo
+        //     {
+        //         Version = "v1",
+        //         Title = "Infotecs Monitoring Service",
+        //         Description = "Infotecs Monitoring Service API",
+        //     });
+        //     // Set the comments path for the Swagger JSON and UI.
+        //     var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        //     var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+        //     c.IncludeXmlComments(xmlPath);
+        // });
         return services;
     }
 }
